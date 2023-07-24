039985b2dca87840e8866cd145297661d14d9499
vlshtm <vandanalshtm@gmail.com>
2023-07-24 09:38:57 +0100

    Distribution graph tooltip css fixed

--- client/src/components/Elements/Graphs/DistributionGraph/DistributionGraphMUI.js
+++ client/src/components/Elements/Graphs/DistributionGraph/DistributionGraphMUI.js
@@ -110,6 +110,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     '@media (max-width: 1000px)': {
       width: '100%',
+      height:'100%',
       overflowY: 'hidden',
       overflowX: 'hidden',
     }
