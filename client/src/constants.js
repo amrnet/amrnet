b499bb835f09cbe6709f2b5ae8f5c7b5e8f625cb
Louise Cerdeira <lcerdeira@gmail.com>
2023-08-01 13:28:35 +0100

    update email and library deps

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 // Static routes to run the app. Only use one: local or heroku.
-//export const API_ENDPOINT = 'http://localhost:8080/api/';
-export const API_ENDPOINT = 'https://amrnet-c321a2c459c0.herokuapp.com/api/';
+export const API_ENDPOINT = 'http://localhost:8080/api/';
+// export const API_ENDPOINT = 'https://amrnetdev.herokuapp.com/api/';
