68129ebd55713a69e019abde61a0bb8bcc47cecc
vlshtm <vandanalshtm@gmail.com>
2023-08-03 13:16:54 +0100

    Rename all the Grey space

--- client/src/components/Elements/Graphs/TrendsKPGraph/TrendsKPGraph.js
+++ client/src/components/Elements/Graphs/TrendsKPGraph/TrendsKPGraph.js
@@ -326,7 +326,7 @@ export const TrendsKPGraph = () => {
               </div>
             </div>
           ) : (
-            <div className={classes.noYearSelected}>No year selected</div>
+            <div className={classes.noYearSelected}>Click on a year to see detail</div>
           )}
         </div>
       </div>
