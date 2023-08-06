926bdb1274a281378b71702f1b5b26882ab1ffae
vlshtm <vandanalshtm@gmail.com>
2023-08-06 22:20:14 +0100

    added search field to new mapView dropdown

--- client/src/components/Elements/Map/TopRightControls2/TopRightControls2.js
+++ client/src/components/Elements/Map/TopRightControls2/TopRightControls2.js
@@ -1,14 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Button, Card, CardContent, Checkbox, ListItemText, MenuItem, Select } from '@mui/material';
+import { Button, Card, CardContent, Checkbox, ListItemText, MenuItem, Select , InputAdornment} from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
 import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../stores/hooks';
 import { setCustomDropdownMapView } from '../../../../stores/slices/graphSlice';
 import { useStyles } from './TopRightControls2MUI';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 
 export const TopRightControls2 = () => {
   const classes = useStyles();
   const [currentTooltip, setCurrentTooltip] = useState(null);
+  const [searchValue2, setSearchValue2] = useState("")
   const dispatch = useAppDispatch();
   const organism = useAppSelector((state) => state.dashboard.organism);
   const genotypesDrugsData2 = useAppSelector((state) => state.graph.genotypesDrugsData2);
@@ -21,13 +25,14 @@ export const TopRightControls2 = () => {
   }, [genotypesDrugsData2, customDropdownMapView]);
 
   function getSelectGenotypeLabel(genotype) {
-    console.log("genotype2",genotype.Susceptible );
+    // console.log("genotype2",genotype.Susceptible );
     const percentage = Number(((genotype.Susceptible / genotype.totalCount) * 100).toFixed(2));
 
     return `${genotype.name} (total N=${genotype.totalCount}, ${percentage}% Susceptible)`;
   }
   
   function getDataForGenotypeSelect() {
+    // console.log("getDataForGenotypeSelect",genotypesDrugsData2);
     return genotypesDrugsData2;
   }
 
@@ -51,17 +56,29 @@ export const TopRightControls2 = () => {
     }
     dispatch(setCustomDropdownMapView(value));
   }
+ function setSearchValue(event){
+  event.preventDefault()
+  setSearchValue2(event.target.value)
+ }
 
+const filteredData = getDataForGenotypeSelect().filter((genotype) =>
+  genotype.name.includes(searchValue2)
+);
+
+
+ console.log("searchValue2", searchValue2);
   return (
     <div className={`${classes.topRightControls}`}>
       <Card elevation={3} className={classes.card}>
         <CardContent className={classes.frequenciesGraph}>
             <Select
               multiple
+              labelId="search-select-label"
+              id="search-select"
               value={customDropdownMapView}
               onChange={(event) => handleChangeSelectedGenotypes({ event })}
-              displayEmpty
               disabled={organism === 'none'}
+              onClose={(e) => setSearchValue2("")}
               endAdornment={
                 <Button
                   variant="outlined"
@@ -73,14 +90,31 @@ export const TopRightControls2 = () => {
                   Clear All
                 </Button>
               }
-              inputProps={{ className: classes.genotypesSelectInput }}
-              MenuProps={{ classes: { paper: classes.genotypesMenuPaper, list: classes.genotypesSelectMenu } }}
+              // inputProps={{ className: classes.genotypesSelectInput }}
+              // MenuProps={{ classes: { paper: classes.genotypesMenuPaper, list: classes.genotypesSelectMenu } }}
               renderValue={(selected) => (
                 <div>{`${selected.length} of ${getDataForGenotypeSelect().length} selected`}</div>
               )}
             >
-              {getDataForGenotypeSelect().map((genotype, index) => (
-                <MenuItem key={`frequencies-option-${index}`} value={genotype.name}>
+              <TextField 
+                size="small"
+                autoFocus
+                placeholder="Type to search..."
+                label="Search genotype" 
+                variant="standard" 
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  )
+                }}
+                sx={{ width:'90%', margin:'0% 5%'}}
+                onChange={e => setSearchValue(e)}
+                onKeyDown={(e) => e.stopPropagation()}
+              />
+              {filteredData.map((genotype, index) => (
+                <MenuItem key={`frequencies-option-${index}`} value={genotype.name} >
                   <Checkbox checked={customDropdownMapView.indexOf(genotype.name) > -1} />
                   <ListItemText primary={getSelectGenotypeLabel(genotype)} />
                 </MenuItem>
