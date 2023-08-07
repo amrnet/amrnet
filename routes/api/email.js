5ce58c55fd3962febecdc141a7d87d151f3e5385
vlshtm <vandanalshtm@gmail.com>
2023-08-07 02:03:44 +0100

    Merge error revert

--- routes/api/email.js
+++ routes/api/email.js
@@ -9,23 +9,20 @@ router.post('/', async function (req, res, next) {
   try {
     const userInfo = req.body;
     const receiverEmail = process.env.EMAIL_USER;
-
-    console.log(process.env.EMAIL_USER, process.env.EMAIL_PASSWD);
+    const receiverPass = process.env.EMAIL_PASSWD;
 
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER, // generated ethereal user
-        pass: process.env.EMAIL_PASSWD // generated ethereal password
+        user: receiverEmail,
+        pass: receiverPass
       }
     });
 
-    const info = await transporter.sendMail({
-      from: `"${userInfo.firstName} ${userInfo.lastName}" <${userInfo.email}>`, // sender address
+    const mailOptions = await transporter.sendMail({
+      from: userInfo.email, // sender address
       to: receiverEmail, // list of receivers
-      subject: `New contact ${userInfo.firstName} ${userInfo.lastName} interetest`, // Subject line
+      subject: `New contact ${userInfo.firstName} ${userInfo.lastName} interested`, // Subject line
       text: '', // plain text body
       html: `
       <p><b>E-mail: </b>${userInfo.email}</p>
@@ -35,8 +32,8 @@ router.post('/', async function (req, res, next) {
     ` // html body
     });
 
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log('Message sent: %s', mailOptions.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(mailOptions));
 
     res.send('Send success!');
   } catch (error) {
