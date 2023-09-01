c98a4f581c700a2a8d1bb572fb8d1fca73beaea9
vlshtm <vandanalshtm@gmail.com>
2023-09-01 14:47:39 +0100

    old API to call data on dashboard

--- routes/api/api.js
+++ routes/api/api.js
@@ -3,21 +3,18 @@ const router = express.Router();
 import csv from 'csv-parser';
 import fs from 'fs';
 import * as Tools from '../../services/services.js';
+import {client} from '../../config/db2.js'
 
 // Get all data from the clean or cleanDB_st file inside assets
-router.get('/getDataFromCSV', function (req, res, next) {
-  let results = [];
-  let read_file = Tools.path_clean_db_st || Tools.path_clean_st;
-
-  fs.createReadStream(read_file)
-    .on('error', (_) => {
-      return res.json([]);
-    })
-    .pipe(csv())
-    .on('data', (data_) => results.push(data_))
-    .on('end', () => {
-      return res.json(results);
-    });
+router.get('/getDataFromCSV', async function (req, res, next) {
+  try {
+        const result = await client.db("amr_t").collection("combine7").find({ 'Exclude': 'Include' }).toArray();;
+        console.log(result.length);
+        return res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 router.get('/getDataFromCSVKlebe', function (req, res, next) {
