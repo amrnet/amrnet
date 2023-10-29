d946b4caf892c4706f00947b243097303b25bd14
vlshtm <vandanalshtm@gmail.com>
2023-10-29 17:09:15 +0000

    Update api

--- routes/api/api.js
+++ routes/api/api.js
@@ -8,7 +8,7 @@ import {client} from '../../config/db2.js'
 // Get all data from the clean or cleanDB_st file inside assets
 router.get('/getDataFromCSV', async function (req, res, next) {
   try {
-        const result = await client.db("salmotyphi").collection("combine7").find({ 'Exclude': 'Include' }).toArray();;
+        const result = await client.db("salmotyphi").collection("mergest").find({ 'Exclude': 'Include' }).toArray();;
         console.log(result.length);
         return res.json(result);
     } catch (error) {
@@ -17,19 +17,15 @@ router.get('/getDataFromCSV', async function (req, res, next) {
     }
 });
 
-router.get('/getDataFromCSVKlebe', function (req, res, next) {
-  let results = [];
-  let read_file = Tools.path_clean_all_kp;
-
-  fs.createReadStream(read_file)
-    .on('error', (_) => {
-      return res.json([]);
-    })
-    .pipe(csv())
-    .on('data', (data_) => results.push(data_))
-    .on('end', () => {
-      return res.json(results);
-    });
+router.get('/getDataFromCSVKlebe', async function (req, res, next) {
+  try {
+        const result = await client.db("salmotyphi").collection("mergekleb").find({ 'Exclude': 'Include' }).toArray();;
+        console.log(result.length);
+        return res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 export default router;
