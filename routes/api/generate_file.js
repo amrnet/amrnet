127fc720dbf76ac8a53e0e4fee6ba2778757a696
Louise Cerdeira <lcerdeira@gmail.com>
2023-11-24 12:39:43 +0000

    fix bugs in AMRnet

--- routes/api/generate_file.js
+++ routes/api/generate_file.js
@@ -615,9 +615,13 @@ router.get('/create', async function (req, res) {
 });
 
 // Download clean as spreadsheet
+// function to handle downloading file
 router.post('/download', function (req, res, next) {
+  
+// get organism from request body
   const organism = req.body.organism;
 
+// determine file path based on organism
   let path_file = '';
 
   if (organism === 'typhi') {
@@ -626,15 +630,21 @@ router.post('/download', function (req, res, next) {
     path_file = Tools.path_clean_all_kp;
   }
 
+// set CORS header for download
   res.setHeader('Access-Control-Allow-Origin', '*');
+// send file as response
   res.download(path_file);
 });
 
 // Get data for admin page: changes and current data
 router.get('/databaseLog', function (req, res, next) {
+// Define file path
   const path = './assets/database/previousDatabases.txt';
+// Read text file contents  
   const text = fs.readFileSync(path, 'utf-8');
+// Parse text file into JSON
   const aux = JSON.parse(text);
+// Send JSON as response
   return res.json(aux);
 });
 
