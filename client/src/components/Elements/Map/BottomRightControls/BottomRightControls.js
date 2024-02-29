554117053b70001358d7dd68cc8349866eb6b2b8
Louise Cerdeira <lcerdeira@gmail.com>
2024-02-29 12:16:16 +0000

    modified:   client/src/components/Dashboard/Dashboard.js 	modified:   client/src/components/Dashboard/filters.js 	modified:   client/src/components/Elements/DownloadData/DownloadData.js 	modified:   client/src/components/Elements/Graphs/DistributionGraph/DistributionGraph.js 	modified:   client/src/components/Elements/Map/BottomRightControls/BottomRightControls.js 	modified:   client/src/components/Elements/Map/Map.js 	new file:   client/src/components/Elements/Map/Ng_mast/Ngmast.js 	new file:   client/src/components/Elements/Map/Ng_mast/NgmastMUI.js 	modified:   client/src/components/Elements/Map/TopRightControls/TopRightControls.js 	modified:   client/src/components/Elements/Map/TopRightControls2/TopRightControls2.js 	modified:   client/src/stores/slices/graphSlice.ts 	modified:   client/src/stores/slices/mapSlice.ts 	modified:   client/src/util/graphCards.js 	modified:   client/src/util/reportInfoTexts.js

--- client/src/components/Elements/Map/BottomRightControls/BottomRightControls.js
+++ client/src/components/Elements/Map/BottomRightControls/BottomRightControls.js
@@ -102,6 +102,14 @@ export const BottomRightControls = () => {
             ctx.fillText('Selected Genotypes: ' + genotypesText, canvas.width / 2, 290);
           }
         }
+        if(mapView === 'NG-MAST prevalence'){
+          if (customDropdownMapView.length === 1) {
+            ctx.fillText('Selected NG-MAST TYPE: ' + customDropdownMapView, canvas.width / 2, 290);
+          } else if (customDropdownMapView.length > 1) {
+            const genotypesText = customDropdownMapView.join(', ');
+            ctx.fillText('Selected NG-MAST TYPE: ' + genotypesText, canvas.width / 2, 290);
+          }
+        }
         ctx.drawImage(mapImg, 0, textHeight+50, canvas.width, cHeight);
 
         const legendImg = document.createElement('img');
@@ -119,6 +127,7 @@ export const BottomRightControls = () => {
           case 'Sensitive to all drugs':
             legendImg.src = 'legends/MapView_Sensitive.png';
             break;
+          case 'NG-MAST prevalence':
           case 'Genotype prevalence':
             legendImg.src = 'legends/MapView_prevalence.png';
             break;
