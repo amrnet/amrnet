0bfa902bc207b4a765f64ad6396d8ce008859298
Louise Cerdeira <lcerdeira@gmail.com>
2024-02-29 12:45:53 +0000

    Merge branch 'wtconfedev3' into dev

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 // Static routes to run the app. Only use one: local or heroku.
 //export const API_ENDPOINT = 'http://localhost:8080/api/';
-export const API_ENDPOINT = 'https://amrnetdev2-bda07af7e807.herokuapp.com/api/';
+export const API_ENDPOINT = 'https://amrnetdev-c321a2c459c0.herokuapp.com/api/';
