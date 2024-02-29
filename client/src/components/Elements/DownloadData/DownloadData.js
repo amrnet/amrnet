632efb3bf8cc1fbbc63c1c1a22b1703dc93dc817
vlshtm <vandanalshtm@gmail.com>
2024-02-29 10:49:59 +0000

    Update Report front page with heading for kpneumo anf ngono

--- client/src/components/Elements/DownloadData/DownloadData.js
+++ client/src/components/Elements/DownloadData/DownloadData.js
@@ -407,14 +407,79 @@ export const DownloadData = () => {
         euFlag.src = EUFlagImg;
         doc.addImage(euFlag, 'JPG',173,pageHeight-38, 12, 7, undefined,'FAST');
       }else if (organism === 'kpneumo') {
+        // Info
+        let newLine = 10;
+        let kleb = 92;
+        doc.text(texts[0], 16, 105, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'bold');
+        doc.text(texts[1], 16, 125, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[2], 16, 135, { align: 'left', maxWidth: pageWidth - 36});
+        doc.setFont(undefined, "italic");
+        doc.text(texts[3], 155, 145, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[4], 247, 145, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, "italic");
+        doc.text(texts[5], 16, 165, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[6], 108, 165, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[7], 16, 175, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'bold').setTextColor(255,0,0);
+        doc.text(texts[8], 16, 205, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[9], 16, 215, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, "italic");
+        doc.text(texts[10], 35, 215, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[11], 35+kleb, 215, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[12], 16, 225, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[13], 16, 235, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[14], 16, 245, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[15], 16, 255, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, "italic");
+        doc.text(texts[16], 240, 255, { align: 'left', maxWidth: pageWidth - 36 });
+         doc.setFont(undefined, 'normal');
+        doc.text(texts[17], 332, 255, { align: 'left', maxWidth: pageWidth - 36});
+        doc.text(texts[18], 16, 265, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, "italic");
+        doc.text(texts[19], 16, 295, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[20], 198, 295, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[21], 16, 305, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setTextColor(0,0,0)
+        doc.text(texts[22], 16, 325, { align: 'left', maxWidth: pageWidth - 36 });
+
+        doc.text(texts[23], 16, 355, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, "italic");
+        doc.text(texts[24], 60, 355, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[25], 60+kleb, 355, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'bold');
+        doc.text(texts[26], 16, 375, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[27], 16, 385, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[28], 16, 395, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[29], 16+kleb, 395, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[30], 16, 405, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[31], 16, 435, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'bold');
+        doc.text(texts[32], 16, 475, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[33], 16, 485, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[34], 16, 495, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[35], 16, 505, { align: 'left', maxWidth: pageWidth - 36 });
+        // const euFlag = new Image();
+        // euFlag.src = EUFlagImg;
+        // doc.addImage(euFlag, 'JPG',173,pageHeight-38, 12, 7, undefined,'FAST');
+      }else if (organism === 'ngono') {
         // let list = PMID.filter((value)=> value !== "-")
         // let pmidSpace, dynamicText;
         // if (actualCountry === 'All'){
         //   pmidSpace = 0;
-        //   dynamicText = `TyphiNET presents data aggregated from >100 studies. Data are drawn from studies with the following PubMed IDs (PMIDs) or Digital Object Identifier (DOI): ${list.join(', ')}.`
+        //   dynamicText = `Data are drawn from studies with the following PubMed IDs (PMIDs) or Digital Object Identifier (DOI): ${list.join(', ')}.`
         // }else{
         //   list = listPIMD.filter((value)=> value !== "-")
-        //   dynamicText = `TyphiNET presents data aggregated from >100 studies. Data for country ${actualCountry} are drawn from studies with the following PubMed IDs (PMIDs) or Digital Object Identifier (DOI): ${list.join(', ')}.`
+        //   dynamicText = `Data for country ${actualCountry} are drawn from studies with the following PubMed IDs (PMIDs) or Digital Object Identifier (DOI): ${list.join(', ')}.`
         //   const textWidth = doc.getTextWidth(dynamicText);
           
         //   const widthRanges = [815, 1200, 1600, 2000, 2400];
@@ -423,98 +488,34 @@ export const DownloadData = () => {
         //     // Find the appropriate pmidSpace based on textWidth
         //     pmidSpace = pmidSpaces.find((space, index) => textWidth <= widthRanges[index]) || pmidSpaces[pmidSpaces.length - 1];
         //   }
-        // doc.text(dynamicText,16, 585,{ align: 'left', maxWidth: pageWidth - 36 });
+        // doc.text(dynamicText,16, 205,{ align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[0], 16, 105, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'bold');
+        doc.text(texts[1], 16, 125, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.setFont(undefined, 'normal');
+        doc.text(texts[2], 16, 135, { align: 'left', maxWidth: pageWidth - 36});
+        doc.text(texts[3], 16, 165, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[4], 16, 205, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[5], 16, 255, { align: 'left', maxWidth: pageWidth - 36 });
         
-        // Info
-        let newLine = 10;
-        let kleb = 92;
-        doc.text(texts[0], 16, 85, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'bold');
-        doc.text(texts[1], 16, 105, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[2], 16, 115, { align: 'left', maxWidth: pageWidth - 36});
-        doc.setFont(undefined, "italic");
-        doc.text(texts[3], 155, 125, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[4], 247, 125, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, "italic");
-        doc.text(texts[5], 16, 145, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[6], 108, 145, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[7], 16, 155, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'bold').setTextColor(255,0,0);
-        doc.text(texts[8], 16, 185, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[9], 16, 195, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, "italic");
-        doc.text(texts[10], 35, 195, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[11], 35+kleb, 195, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[12], 16, 205, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[13], 16, 215, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[14], 16, 225, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[15], 16, 235, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, "italic");
-        doc.text(texts[16], 240, 235, { align: 'left', maxWidth: pageWidth - 36 });
-         doc.setFont(undefined, 'normal');
-        doc.text(texts[17], 332, 235, { align: 'left', maxWidth: pageWidth - 36});
-        doc.text(texts[18], 16, 245, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, "italic");
-        doc.text(texts[19], 16, 275, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[20], 198, 275, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[21], 16, 285, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setTextColor(0,0,0)
-        doc.text(texts[22], 16, 305, { align: 'left', maxWidth: pageWidth - 36 });
-
-        doc.text(texts[23], 16, 335, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, "italic");
-        doc.text(texts[24], 60, 335, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[25], 60+kleb, 335, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'bold');
-        doc.text(texts[26], 16, 355, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[27], 16, 365, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[28], 16, 375, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[29], 16+kleb, 375, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[30], 16, 385, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[31], 16, 415, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'bold');
-        doc.text(texts[32], 16, 455, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[33], 16, 465, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[34], 16, 475, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[35], 16, 485, { align: 'left', maxWidth: pageWidth - 36 });
-        // const euFlag = new Image();
-        // euFlag.src = EUFlagImg;
-        // doc.addImage(euFlag, 'JPG',173,pageHeight-38, 12, 7, undefined,'FAST');
-      }else if (organism === 'ngono') {
-        let kleb = 92;
-        doc.text(texts[0], 16, 85, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'bold');
-        doc.text(texts[1], 16, 105, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.setFont(undefined, 'normal');
-        doc.text(texts[2], 16, 115, { align: 'left', maxWidth: pageWidth - 36});
-        doc.text(texts[3], 16, 145, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[4], 16, 185, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[5], 16, 235, { align: 'left', maxWidth: pageWidth - 36 });
-        
-        doc.text(texts[6], 16, 265, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[6], 16, 285, { align: 'left', maxWidth: pageWidth - 36 });
         doc.setFont(undefined, 'bold');
       
-        doc.text(texts[7], 16, 285, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[7], 16, 305, { align: 'left', maxWidth: pageWidth - 36 });
         doc.setFont(undefined, 'normal');
-        doc.text(texts[8], 16, 305, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[9], 16, 365, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[10], 16, 405, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[8], 16, 325, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[9], 16, 385, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[10], 16, 425, { align: 'left', maxWidth: pageWidth - 36 });
         doc.setFont(undefined, 'bold');
-        doc.text(texts[11], 16, 445, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[11], 16, 465, { align: 'left', maxWidth: pageWidth - 36 });
         doc.setFont(undefined, 'normal');
-        doc.text(texts[12], 16, 465, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[13], 16, 505, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[14], 16, 535, { align: 'left', maxWidth: pageWidth - 36 });
-        doc.text(texts[15], 16, 575, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[12], 16, 485, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[13], 16, 525, { align: 'left', maxWidth: pageWidth - 36 });
+        doc.text(texts[14], 16, 555, { align: 'left', maxWidth: pageWidth - 36 });
+        drawFooter({ document: doc, pageHeight, pageWidth, date });
+        doc.addPage();
+        drawHeader({ document: doc, pageWidth});
+        doc.text(texts[15], 16, 44, { align: 'left', maxWidth: pageWidth - 36 });
 
       } 
 
