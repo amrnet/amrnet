042b63a4ed10432f8b93977a28a965fd845e1310
vlshtm <vandanalshtm@gmail.com>
2024-03-01 22:34:36 +0000

    Team card added

--- client/src/components/About/AboutMUI.js
+++ client/src/components/About/AboutMUI.js
@@ -5,13 +5,24 @@ const useStyles = makeStyles((theme) => ({
     '&.MuiCard-root': {
       borderRadius: '16px'
     },
-    minHeight: '500px'
+    // minHeight: '500px'
   },
   cardContent: {
     margin: '0px 2%'
   },
   paragraph: {
     textAlign: 'justify'
+  },
+  teamHeading: {
+    textAlign: 'center'
+  },
+  teamMember:{
+    width: 270, 
+    height: 200, 
+    border:"2px solid gray", 
+    borderRadius:"5px",
+    padding:"20px",
+    margin:"20px !important"
   }
 }));
 
