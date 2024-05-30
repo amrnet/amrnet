016d868e98d43517c23e2e9ec221c498f6eec7fd
Louise Cerdeira <lcerdeira@gmail.com>
2024-05-30 16:42:29 -0300

    new build RD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,8 +49,8 @@ html_theme_options = {
     'logo_only': True,
     'display_version': False,
     'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
     'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
