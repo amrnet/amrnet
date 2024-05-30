e4d4b2c8ef576cd6233430cfdaa9023f0dcbaa43
Louise Cerdeira <lcerdeira@gmail.com>
2024-05-30 10:13:12 -0300

    update RD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,12 +65,12 @@ epub_show_urls = 'footnote'
 html_theme_options = {
     'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
     'analytics_anonymize_ip': False,
-    'logo_only': False,
+    'logo_only': True,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': 'blue',
+    'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
