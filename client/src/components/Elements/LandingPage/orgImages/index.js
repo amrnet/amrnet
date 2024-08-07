35713d440a50ab6440322d79e8aedf034ccaf0db
vlshtm <vandanalshtm@gmail.com>
2024-08-07 14:59:38 +0100

    Updated landing page

---
+++ client/src/components/Elements/LandingPage/orgImages/index.js
@@ -0,0 +1,8 @@
+import typhi from './typhi.jpeg';
+import shig from './shig.jpeg';
+import ngono from './ngono.jpg';
+import decoli from './decoli.jpg';
+import ints from './ints.jpeg';
+import kleb from './kleb.jpg';
+
+export { typhi, shig, ngono, decoli, ints, kleb };
