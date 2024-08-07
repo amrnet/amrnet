4e953c4e0cc2ebe23203c3f5489af95ca8078626
vlshtm <vandanalshtm@gmail.com>
2024-08-07 14:59:38 +0100

    Updated landing page

--- client/src/util/orgCard.js
+++ client/src/util/orgCard.js
@@ -1,42 +1,12 @@
+import { typhi, shig, ngono, decoli, ints, kleb } from '../components/Elements/LandingPage/orgImages';
+
 export const orgCard = [
-  {
-    label: 'Salmonella Typhi',
-    value: 'styphi',
-    abbr: 'S. Typhi',
-  },
-  {
-    label: 'Klebsiella pneumoniae',
-    value: 'kpneumo',
-    abbr: 'K. pneumoniae',
-  },
-  {
-    label: 'Neisseria gonorrhoeae',
-    value: 'ngono',
-    abbr: 'N. gonorrhoeae',
-  },
-  // {
-  //   label: 'Escherichia coli',
-  //   value: 'ecoli',
-  //   abbr: 'E. coli'
-  // },
-  {
-    label: 'Diarrheagenic E. coli',
-    value: 'decoli',
-    abbr: 'DEC',
-  },
-  {
-    label: 'Shigella + EIEC',
-    value: 'shige',
-    abbr: 'Shigella+EIEC',
-  },
-  {
-    label: 'Invasive non-typhoidal Salmonella',
-    value: 'sentericaints',
-    abbr: 'iNTS',
-  },
-  // {
-  //   label: 'Salmonella enterica',
-  //   value: 'senterica',
-  //   abbr: 'S. enterica'
-  // }
+  { label: 'Salmonella Typhi', value: 'styphi', abbr: 'S. Typhi', img: typhi },
+  { label: 'Klebsiella pneumoniae', value: 'kpneumo', abbr: 'K. pneumoniae', img: kleb },
+  { label: 'Neisseria gonorrhoeae', value: 'ngono', abbr: 'N. gonorrhoeae', img: ngono },
+  // { label: 'Escherichia coli', value: 'ecoli', abbr: 'E. coli' },
+  { label: 'Diarrheagenic E. coli', value: 'decoli', abbr: 'DEC', img: decoli },
+  { label: 'Shigella + EIEC', value: 'shige', abbr: 'Shigella+EIEC', img: shig },
+  { label: 'Invasive non-typhoidal Salmonella', value: 'sentericaints', abbr: 'iNTS', img: ints },
+  // { label: 'Salmonella enterica', value: 'senterica', abbr: 'S. enterica' }
 ];
