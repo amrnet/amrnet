086efbfe54302b1e691273a38ad334af8c0742cf
Louise Cerdeira <lcerdeira@gmail.com>
2024-09-13 13:07:56 +0100

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,3 @@
 // Static routes to run the app. Only use one: local or heroku.
 //export const API_ENDPOINT = 'http://localhost:8080/api/';
-export const API_ENDPOINT = 'https://amrnet-d7387c560cfc.herokuapp.com/api/';
-
+export const API_ENDPOINT = 'https://amrnetdev2-bda07af7e807.herokuapp.com/api/';
