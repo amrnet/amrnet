5ca11437b9f7cc70bc8f9990a718b08ecffbff05
Louise Cerdeira <lcerdeira@gmail.com>
2024-11-04 15:12:53 +0000

    fix changes previous PR

--- client/src/util/convergenceVariablesOptions.js
+++ client/src/util/convergenceVariablesOptions.js
@@ -1,10 +1,10 @@
 export const variablesOptions = [
   { label: 'Country', value: 'COUNTRY_ONLY' },
   { label: 'Year', value: 'DATE' },
-  { label: 'Genotype', value: 'GENOTYPE' },
+  { label: 'Genotype (7-locus MLST)', value: 'GENOTYPE' },
   { label: 'cgST', value: 'cgST' },
-  // { label: 'Yersiniabactin', value: 'Yersiniabactin' },
-  // { label: 'Host', value: 'Host' },
+  { label: 'Sublineage (lincode associated)', value: 'Sublineage' },
+  { label: 'Host', value: 'Host' },
   // { label: 'Bla_Carb_acquired', value: 'Bla_Carb_acquired' },
   // { label: 'Bla_ESBL_acquired', value: 'Bla_ESBL_acquired' }
 ];
