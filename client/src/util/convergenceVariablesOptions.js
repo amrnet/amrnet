d49caa4c53ee0539c527c3965e0cf893ef664d15
vlshtm <vandanalshtm@gmail.com>
2024-11-06 17:58:24 +0530

    Change 'cgST' to 'cgST (core genome MLST)' in the menu

--- client/src/util/convergenceVariablesOptions.js
+++ client/src/util/convergenceVariablesOptions.js
@@ -2,7 +2,7 @@ export const variablesOptions = [
   { label: 'Country', value: 'COUNTRY_ONLY' },
   { label: 'Year', value: 'DATE' },
   { label: 'Genotype (7-locus MLST)', value: 'GENOTYPE' },
-  { label: 'cgST', value: 'cgST' },
+  { label: 'cgST (core genome MLST)', value: 'cgST' },
   { label: 'Sublineage', value: 'Sublineage' },
   { label: 'Host', value: 'Host' },
   // { label: 'Bla_Carb_acquired', value: 'Bla_Carb_acquired' },
