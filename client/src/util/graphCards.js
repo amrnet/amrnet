c50611ee40d1df1733f71ed1662d939acf3f063f
13vandana <vandanalshtm@gmail.com>
2025-03-19 01:13:22 +0000

    Rename graph cards and remove RFWG plots

--- client/src/util/graphCards.js
+++ client/src/util/graphCards.js
@@ -21,31 +21,31 @@ function getTrendstitle(organism) {
 
 export const graphCards = [
   {
-    title: 'Drug resistance trends',
+    title: 'AMR trends',
     description: ['Data are plotted for years with N ≥ 10 genomes'],
     icon: <Timeline color="primary" />,
     id: 'DRT',
     organisms: ['styphi', 'ngono', 'kpneumo', 'sentericaints'],
     component: <DrugResistanceGraph />,
   },
+  // {
+  //   title: 'Resistance frequencies within genotypes',
+  //   description: ['Top Genotypes (up to 7)'],
+  //   icon: <BarChart color="primary" />,
+  //   id: 'RFWG',
+  //   organisms: ['styphi', 'kpneumo', 'ngono'],
+  //   component: <FrequenciesGraph />,
+  // },
+  // {
+  //   title: 'Resistance frequencies within lineages',
+  //   description: ['Top Lineages (up to 7)'],
+  //   icon: <BarChart color="primary" />,
+  //   id: 'RFWG',
+  //   organisms: ['sentericaints'],
+  //   component: <FrequenciesGraph />,
+  // },
   {
-    title: 'Resistance frequencies within genotypes',
-    description: ['Top Genotypes (up to 7)'],
-    icon: <BarChart color="primary" />,
-    id: 'RFWG',
-    organisms: ['styphi', 'kpneumo', 'ngono'],
-    component: <FrequenciesGraph />,
-  },
-  {
-    title: 'Resistance frequencies within lineages',
-    description: ['Top Lineages (up to 7)'],
-    icon: <BarChart color="primary" />,
-    id: 'RFWG',
-    organisms: ['sentericaints'],
-    component: <FrequenciesGraph />,
-  },
-  {
-    title: 'Resistance determinants within genotypes',
+    title: 'AMR markers by genotype',
     description: ['Top Genotypes (up to 10)'],
     icon: <StackedBarChart color="primary" />,
     id: 'RDWG',
@@ -53,7 +53,7 @@ export const graphCards = [
     component: <DeterminantsGraph />,
   },
   {
-    title: 'Genotype distribution',
+    title: 'Genotype trends',
     description: [''],
     icon: <StackedBarChart color="primary" />,
     id: 'GD',
@@ -118,7 +118,7 @@ export const graphCards = [
   //   component: <BubbleHeatmapGraph />,
   // },
   {
-    title: 'Heatmap ST vs genotype',
+    title: 'AMR by genotype',
     description: [''],
     icon: <BubbleChart color="primary" />,
     id: 'HSG2',
