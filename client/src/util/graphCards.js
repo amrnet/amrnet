2dca4f61d101b0d91714703d574662966d1ccb2e
13vandana <vandanalshtm@gmail.com>
2025-03-19 14:16:00 +0000

    All other plot re-name

--- client/src/util/graphCards.js
+++ client/src/util/graphCards.js
@@ -7,7 +7,21 @@ import { TrendsGraph } from '../components/Elements/Graphs/TrendsGraph';
 import { KODiversityGraph } from '../components/Elements/Graphs/KODiversityGraph';
 import { ConvergenceGraph } from '../components/Elements/Graphs/ConvergenceGraph';
 import { BubbleHeatmapGraph2 } from '../components/Elements/Graphs/BubbleHeatmapGraph2';
+import { useAppSelector } from '../stores/hooks';
 
+
+
+
+function getHeatMapsTitle(organism) {
+  switch (organism) {
+    case 'kpneumo':
+      return 'AMR by ST';
+    case 'ints':
+      return 'AMR by lineage';
+    default:
+      return 'AMR by genotype';
+  }
+}
 function getTrendstitle(organism) {
   switch (organism) {
     case 'kpneumo':
@@ -49,7 +63,15 @@ export const graphCards = [
     description: ['Top Genotypes (up to 10)'],
     icon: <StackedBarChart color="primary" />,
     id: 'RDWG',
-    organisms: ['styphi', 'kpneumo', 'ngono'],
+    organisms: ['styphi', 'ngono'],
+    component: <DeterminantsGraph />,
+  },
+  {
+    title: 'AMR markers by ST',
+    description: ['Top Genotypes (up to 10)'],
+    icon: <StackedBarChart color="primary" />,
+    id: 'RDWG',
+    organisms: ['kpneumo'],
     component: <DeterminantsGraph />,
   },
   {
@@ -70,7 +92,7 @@ export const graphCards = [
   },
   {
     //TODO: add this chart above because this chart is a distribution chart, which means is the same plot, only the title will change(same id as well)
-    title: 'Lineage prevalence',
+    title: 'Lineage trends',
     description: [''],
     icon: <StackedBarChart color="primary" />,
     id: 'GD',
@@ -118,16 +140,40 @@ export const graphCards = [
   //   component: <BubbleHeatmapGraph />,
   // },
   {
-    title: 'AMR by genotype',
+    title: getHeatMapsTitle('styphi'),
     description: [''],
     icon: <BubbleChart color="primary" />,
     id: 'HSG2',
-    organisms: ['styphi', 'ngono', 'kpneumo', 'shige', 'decoli', 'ecoli', 'sentericaints'],
+    organisms: ['styphi', 'ngono', 'shige', 'decoli', 'ecoli'],
+    component: <BubbleHeatmapGraph2 />,
+  },
+  {
+    title: getHeatMapsTitle('ints'),
+    description: [''],
+    icon: <BubbleChart color="primary" />,
+    id: 'HSG2',
+    organisms: ['sentericaints'],
+    component: <BubbleHeatmapGraph2 />,
+  },
+  {
+    title: getHeatMapsTitle('kpneumo'),
+    description: [''],
+    icon: <BubbleChart color="primary" />,
+    id: 'HSG2',
+    organisms: ['kpneumo'],
     component: <BubbleHeatmapGraph2 />,
   },
 ];
 
 export const continentGraphCard = {
+  title: 'Geographic Comparisons',
+  description: [''],
+  icon: <BubbleChart color="primary" />,
+  id: 'CGC',
+  organisms: ['styphi', 'ngono', 'kpneumo'],
+};
+
+export const mapViewCard = {
   title: 'Geographic Comparisons',
   description: [''],
   icon: <BubbleChart color="primary" />,
