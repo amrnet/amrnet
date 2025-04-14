baf46c87e05da323c7166f4ee7ab1ec0134f5a82
13vandana <vandanalshtm@gmail.com>
2025-04-14 10:10:18 +0100

    removed fixed GLOBAL FILTER from map view

--- client/src/components/Elements/Map/Map.js
+++ client/src/components/Elements/Map/Map.js
@@ -569,7 +569,7 @@ export const Map = () => {
           </ComposableMap>
           {!matches && (
             <>
-              <TopLeftControls />
+              {/* <TopLeftControls /> */}
               <TopRightControls />
               {['Genotype prevalence', 'ST prevalence', 'Lineage prevalence', 'Resistance prevalence'].includes(
                 mapView,
@@ -593,7 +593,7 @@ export const Map = () => {
             ) : mapView === 'NG-MAST prevalence' ? (
               <Ngmast />
             ) : null}
-            <TopLeftControls />
+            {/* <TopLeftControls /> */}
           </div>
         )}
         <ReactTooltip>
