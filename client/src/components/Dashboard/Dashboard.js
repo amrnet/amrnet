d5ee6f4850cd7adf9c712efeeabfc4522729a816
13vandana <vandanalshtm@gmail.com>
2025-05-02 13:05:25 +0100

    Resistance prevalence default in Map view for all organisms and scale (error continue...)
    (cherry picked from commit 7f37c15e77d3825542326331e5dd2792e8fc0013)

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -396,7 +396,7 @@ export const DashboardPage = () => {
           dispatch(setDeterminantsGraphDrugClass('Ciprofloxacin NS'));
           break;
         case 'kpneumo':
-          dispatch(setMapView('No. Samples'));
+          dispatch(setMapView('Resistance prevalence'));
           dispatch(setDrugResistanceGraphView(drugsKP));
           dispatch(setDeterminantsGraphDrugClass('Carbapenems'));
           dispatch(setTrendsGraphDrugClass('Carbapenems'));
@@ -408,21 +408,21 @@ export const DashboardPage = () => {
           // setCurrentConvergenceColourVariable('DATE');
           break;
         case 'ngono':
-          dispatch(setMapView('No. Samples'));
+          dispatch(setMapView('Resistance prevalence'));
           dispatch(setDrugResistanceGraphView(defaultDrugsForDrugResistanceGraphNG));
           dispatch(setDeterminantsGraphDrugClass('Azithromycin'));
           dispatch(setTrendsGraphDrugClass('Azithromycin'));
           dispatch(setTrendsGraphView('number'));
           break;
         case 'sentericaints':
-          dispatch(setMapView('No. Samples'));
+          dispatch(setMapView('Resistance prevalence'));
           dispatch(setDrugResistanceGraphView(drugsINTS));
           break;
         case 'ecoli':
         case 'decoli':
         case 'shige':
         case 'senterica':
-          dispatch(setMapView('No. Samples'));
+          dispatch(setMapView('Resistance prevalence'));
           dispatch(setDrugResistanceGraphView(drugsINTS));
           break;
         default:
