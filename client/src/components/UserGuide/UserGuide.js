7ec96b81a01b66f2761aeff6a6963dfed895dfc5
Louise Cerdeira <lcerdeira@gmail.com>
2025-08-14 13:22:39 -0300

    Refactor DownloadData component to support TSV format and improve data handling
    - Updated DownloadData.js to change download format from CSV to TSV.
    - Modified UserGuide.js to reflect the change in download format.
    - Enhanced optimizedDataService.js to implement pagination for Kpneumo and improve caching strategy.
    - Adjusted API routes to support pagination and return structured responses for Kpneumo and Ecoli.
    - Improved MainLayout.js to include a loading message during data fetch.
    - Refactored generateDataAPIsFile.js to ensure consistent file naming and handling.
    - Updated quickPaginationFix.js to enforce pagination for Kpneumo regardless of dataset size.

--- client/src/components/UserGuide/UserGuide.js
+++ client/src/components/UserGuide/UserGuide.js
@@ -81,7 +81,7 @@ export const UserGuidePage = () => {
           <br />
           <Typography variant="body2" className={classes.paragraph}>
             <span className={classes.paragraphBold}>Downloads:</span> At the bottom are buttons to download (1) the
-            individual genome-level information that is used to populate the dashboard (‘Download database (CSV
+            individual genome-level information that is used to populate the dashboard (‘Download database (TSV
             format)’); and (2) a static report of the currently displayed plots, together with a basic description of
             the data sources and variable definitions (‘Download PDF’).
           </Typography>
