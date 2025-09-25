f352bbc5ae7393956e1eca31f89021d4d4061ac2
Louise Cerdeira <lcerdeira@gmail.com>
2025-09-25 18:17:24 -0300

    Add documentation and user guides for AMRnet
    - Created new documentation files for user tutorials, guides, and specific pathogen data.
    - Added citation information for AMRnet in the right.rst file.
    - Implemented a comprehensive user tutorial covering platform usage, pathogen selection, and data interpretation.
    - Introduced specific documentation for *Salmonella* (non-typhoidal), *Salmonella* (invasive non-typhoidal), *Shigella*, and *Salmonella* Typhi.
    - Updated source database references and included variable definitions for clarity.
    - Enhanced the API with rate limiting adjustments and increased aggregation timeout.
    - Updated package dependencies for improved functionality and security.

--- client/src/components/Elements/ResetButton/ResetButton.js
+++ client/src/components/Elements/ResetButton/ResetButton.js
@@ -90,7 +90,7 @@ export const ResetButton = () => {
 
     if (organism === 'styphi') {
       dispatch(setMapView('Resistance prevalence'));
-      dispatch(setDeterminantsGraphDrugClass('Ciprofloxacin NS'));
+      dispatch(setDeterminantsGraphDrugClass('Ciprofloxacin'));
       dispatch(setDrugResistanceGraphView(defaultDrugsForDrugResistanceGraphST));
       dispatch(setCurrentSliderValue(20)); // to reset the genotype trend slider value for styphi
       dispatch(setMapView('Resistance prevalence'));
@@ -99,6 +99,7 @@ export const ResetButton = () => {
       // dispatch(setTrendsGraphDrugClass('Azithromycin'));
       dispatch(setTrendsGraphView('percentage'));
       dispatch(setConvergenceColourPallete({}));
+      dispatch(setBubbleMarkersYAxisType('Ciprofloxacin'));
       // dispatch(setNgmastDrugsData(ngmastData.ngmastDrugData));
       // dispatch(setCustomDropdownMapViewNG(ngmastData.ngmastDrugData.slice(0, 1).map(x => x.name)));
     } else {
@@ -116,6 +117,7 @@ export const ResetButton = () => {
       dispatch(setCurrentSliderValueKOT(20));
       dispatch(setCurrentSliderValueKP_GT(20));
       dispatch(setCurrentSliderValueKP_GE(20));
+      dispatch(setBubbleMarkersYAxisType(markersDrugsKP[0]));
     }
     if (['shige', 'decoli', 'sentericaints'].includes(organism)) {
       dispatch(setSelectedLineages(pathovar));
@@ -138,7 +140,7 @@ export const ResetButton = () => {
     dispatch(setBubbleKOHeatmapGraphVariable('GENOTYPE'));
     dispatch(setBubbleKOYAxisType('O_locus'));
     dispatch(setBubbleMarkersHeatmapGraphVariable('GENOTYPE'));
-    dispatch(setBubbleMarkersYAxisType(markersDrugsKP[0]));
+    
 
     if (organism === 'ngono') {
       dispatch(setCurrentSliderValueRD(maxSliderValueRD));
