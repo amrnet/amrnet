91fe2196de0947e477b2cb072ea82bc2508ecb82
Louise Cerdeira <lcerdeira@gmail.com>
2026-02-18 15:11:08 +0000

    Update constants.js
    Signed-off-by: Louise Cerdeira <lcerdeira@gmail.com>

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 // Static routes to run the app. Uses environment variable or fallback to staging
 // For local development, set REACT_APP_API_URL=http://localhost:8080/api/ in your .env
-export const API_ENDPOINT = process.env.REACT_APP_API_URL || 'https://amrnetdev2-bda07af7e807.herokuapp.com/api/';
+export const API_ENDPOINT = process.env.REACT_APP_API_URL || 'https://amrnetstaging-4f7a1b5cd3f3.herokuapp.com/api/';
