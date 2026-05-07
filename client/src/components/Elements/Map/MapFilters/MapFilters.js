b81dfade284f786201e963efa40f4104bfed022e
vandana <vandanalshtm@gmail.com>
2026-05-07 10:52:56 +0100

    set dafault drug for SP mpaView is Erythromycin

--- client/src/components/Elements/Map/MapFilters/MapFilters.js
+++ client/src/components/Elements/Map/MapFilters/MapFilters.js
@@ -187,7 +187,7 @@ export const MapFilters = ({ showFilter, setShowFilter }) => {
           dispatch(setPrevalenceMapViewOptionsSelected(['Ciprofloxacin NS']));
           break;
         case 'strepneumo':
-          dispatch(setPrevalenceMapViewOptionsSelected(['Co-Trimoxazole']));
+          dispatch(setPrevalenceMapViewOptionsSelected(['Erythromycin']));
           break;
         case 'saureus':
           dispatch(setPrevalenceMapViewOptionsSelected(['Methicillin']));
