51b7e8c4d25ca70bebbbe079d4d96cb265f895cd
vandana <vandanalshtm@gmail.com>
2026-06-01 22:59:45 +0100

    Default drugs for S. aureus

--- client/src/components/Elements/ResetButton/ResetButton.js
+++ client/src/components/Elements/ResetButton/ResetButton.js
@@ -44,6 +44,7 @@ import {
 import { setDataset, setDatasetKP, setMapView, setPosition } from '../../../stores/slices/mapSlice';
 import {
   defaultDrugsForDrugResistanceGraphNG,
+  defaultDrugsForDrugResistanceGraphSA,
   defaultDrugsForDrugResistanceGraphST,
   drugsECOLI,
   drugsKP,
@@ -153,7 +154,7 @@ export const ResetButton = () => {
       dispatch(setDeterminantsGraphDrugClass('Azithromycin'));
     }
     if (organism === 'saureus') {
-      dispatch(setDrugResistanceGraphView(drugsSA));
+      dispatch(setDrugResistanceGraphView(defaultDrugsForDrugResistanceGraphSA));
     }
     if (organism === 'strepneumo') {
       dispatch(setDrugResistanceGraphView(drugsSP));
